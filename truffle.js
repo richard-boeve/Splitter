8eddf8413deb41a4a2dc740566dca187627ab4f9
Richard-Boeve <richardboeve@hotmail.com>
2018-11-27 18:13:37 +1300

    setting up files and taking out specific Metacoin code

--- truffle.js
+++ truffle.js
@@ -1,4 +1,12 @@
+const Web3 = require("web3");
+const net = require("net");
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    ropsten: {
+      provider: new Web3.providers.IpcProvider(process.env['HOME'] + "/.ethereum/testnet/data/geth.ipc", net),
+      network_id: 3,
+      gas: 500000
+    }
+  }
 };
