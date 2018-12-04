116a8c7bc2470e5f542cb42b5b5d383473ed431a
Richard-Boeve <richardboeve@hotmail.com>
2018-12-04 20:29:59 +1300

    fixed the migration mess, now deploying to ganache ok

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,5 +3,5 @@ var Migrations = artifacts.require("./Splitter.sol");
 module.exports = function(deployer, network, accounts) {
   console.log("network:", network);
   console.log("accounts:", accounts);
-  deployer.deploy(Splitter);
+  deployer.deploy(Migrations);
 };
--- truffle.js
+++ truffle.js
@@ -3,10 +3,10 @@ const net = require("net");
 
 module.exports = {
   networks: {
-    ropsten: {
-      provider: new Web3.providers.IpcProvider(process.env['HOME'] + "/.ethereum/testnet/data/geth.ipc", net),
-      network_id: 3,
-      gas: 500000
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
     }
   }
 };
