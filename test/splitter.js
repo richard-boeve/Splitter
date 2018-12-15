02d9c0e67b1673beda1b61a5906a08b0d674596d
Richard-Boeve <richardboeve@hotmail.com>
2018-12-15 18:34:24 +1300

    3 tests created

--- test/splitter.js
+++ test/splitter.js
@@ -25,14 +25,33 @@ contract('Splitter', function (accounts) {
     owner = accounts[0];
     receiver1 = accounts[1];
     receiver2 = accounts[2];
-    it("should deposit to contract", function () {
+    it("owner can deposit to contract", function () {
     var amount = 3;
     return Splitter.deployed().then(function (instance) {
-      return instance.deposit(receiver1, receiver2, { from: owner, value: amount });
-    }).then(function (instance) {
-      return instance.getContractBalance.call(instance);
-    }).then(function (balance) {
-       assert.equal(balanceC.toString(10), "3", "0 is not the balance of the contract");
+      splitter = instance;
+      return splitter.deposit(receiver1, receiver2, { from: owner, value: amount });
+    }).then(function () {
+       return splitter.getContractBalance.call(splitter);
+    }).then(function (balanceC) {
+       assert.equal(balanceC.toString(10), "3", "The contract balance is incorrect");
     });
   });
 })
+
+// contract('Splitter', function (accounts) {
+//   owner = accounts[0];
+//   receiver1 = accounts[1];
+//   receiver2 = accounts[2];
+//   balanceReceiver1 = 1.5;
+//   it("verifies that receiver can withdraw funds", function () {
+//   var amount = 1.5;
+//   return Splitter.deployed().then(function (instance) {
+//     splitter = instance;
+//     return splitter.withdrawFunds({from: accounts[1]});
+//   }).then(function () {
+//     return splitter.getContractBalance.call(splitter);
+//   }).then(function (balanceC) {
+//      assert.equal(balanceC.toString(10), "3", "0 is not the balance of the contract");
+//   });
+// });
+// })
