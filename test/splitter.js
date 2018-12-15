6788b576d434673096df583e83ed901429d846f7
Richard-Boeve <richardboeve@hotmail.com>
2018-12-15 17:47:56 +1300

    truffle-contract module issue

--- test/splitter.js
+++ test/splitter.js
@@ -1,7 +1,5 @@
 var Splitter = artifacts.require("./Splitter.sol");
 
-
-
 contract('Splitter', function () {
   it("should return address of splitter contract deployed", function () {
     return Splitter.deployed().then(function (instance) {
@@ -23,13 +21,18 @@ contract('Splitter', function () {
   });
 })
 
-contract('Splitter', function () {
-  it("should deposit to contract", function () {
+contract('Splitter', function (accounts) {
+    owner = accounts[0];
+    receiver1 = accounts[1];
+    receiver2 = accounts[2];
+    it("should deposit to contract", function () {
+    var amount = 3;
     return Splitter.deployed().then(function (instance) {
-      return instance.deposit;
-    }).then(function (balanceC) {
-      console.log(balanceC.toString(10));
-      assert.equal(balanceC.toString(10), "0", "0 is not the balance of the contract");
+      return instance.deposit(receiver1, receiver2, { from: owner, value: amount });
+    }).then(function (instance) {
+      return instance.getContractBalance.call(instance);
+    }).then(function (balance) {
+       assert.equal(balanceC.toString(10), "3", "0 is not the balance of the contract");
     });
   });
 })
