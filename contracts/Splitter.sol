79444fefe91e925266acaf6c97d8f3948e3c3587
Richard-Boeve <richardboeve@hotmail.com>
2019-01-08 14:20:22 +1300

    new validations during deposit

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -22,12 +22,24 @@ contract Splitter {
 
 
     function deposit(address receiver1, address receiver2) public payable returns (bool){
-        require(msg.sender == aliceOwner, "Only Alice can deposit Ether");
+        require(msg.sender == aliceOwner,"Only Alice can deposit Ether");
+        // Verify non empty addresses have been provided
         require(receiver1 != address(0), "Must provide two addresses");
         require(receiver2 != address(0), "Must provide two addresses");
+        // Verify receiver 1 address is not equal to receiver2 address  
+        require(receiver1 != receiver2, "Must provide 2 different addresses");
+        // Verify if this is second deposit that the addresses provided are still the same
+        if ((bob == address(0) && (carol == address(0)))) {
+            // First time of deposit operation set bob and carol addresses
+            bob = receiver1;
+            carol = receiver2;
+            }
+        else {
+            // Not first time deposit check the addresses provided are still correct
+            require(bob == receiver1 || bob == receiver2,"Address for bob provided is invalid");
+            require(carol == receiver1 || carol == receiver2,"Address for carol provided is invalid");
+            }
         emit LogDeposit(msg.sender, msg.value, receiver1, receiver2);
-        bob = receiver1;
-        carol = receiver2;
         balanceBob += msg.value /2;
         balanceCarol += msg.value /2;
         return true;
@@ -49,4 +61,4 @@ contract Splitter {
     
     function() external {
     }
-}    
+}
