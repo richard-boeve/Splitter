c3d50db2651caba3ce31d4825706950008d57045
Richard-Boeve <richardboeve@hotmail.com>
2019-01-08 20:29:48 +1300

    updated tests and added comments to splitter.js

--- app/splitter.js
+++ app/splitter.js
@@ -6,9 +6,11 @@ const splitterJson = require("../build/contracts/Splitter.json");
 
 require("file-loader?name=./index.html!./index.html");
 
+// Use a web3 browser if availble
 if (typeof web3 !== 'undefined') {
     console.log('Web3 browser detected! ' + web3.currentProvider.constructor.name)
     web3 = new Web3(web3.currentProvider);
+// Otherwise, use a own provider with port 8545  
 } else {
     console.log('Web3 browser not detected, setting own provider!')
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
@@ -20,18 +22,23 @@ Promise.promisifyAll(web3.version, { suffix: "Promise" });
 const Splitter = contract(splitterJson);
 Splitter.setProvider(web3.currentProvider);
 
+// Upon loading of the site 
 window.addEventListener('load', function () {
+    //check if accounts are available
     return web3.eth.getAccountsPromise()
         .then(accounts => {
             console.log(accounts);
+            //when no accounts available, return error
             if (accounts.length == 0) {
                 $("#balanceAlice").html("N/A");
                 throw new Error("No account with which to transact");
             }
+            //if accounts available, set the first one to window account
             window.account = accounts[0];
             console.log("Account:", window.account);
             return web3.eth.getBalancePromise(window.account);
         })
+        // window account to populate Alice's balance
         .then(balance => $("#balanceAlice").html(web3.fromWei(balance.valueOf())))
         .then(() => $("#send").click(deposit))
         .then(() => $("#withdrawBob").click(withdrawFundsBob))
@@ -39,38 +46,49 @@ window.addEventListener('load', function () {
         .catch(console.error);
 });
 
+// Upon loading of the site 
 window.addEventListener('load', function () {
+    //check if accounts are available
     return web3.eth.getAccountsPromise()
         .then(accounts => {
+            //when no accounts available, return error
             if (accounts.length == 0) {
                 $("#balanceBob").html("N/A");
                 throw new Error("No account with which to transact");
             }
+            //if accounts available, set the second one to window account
             window.account = accounts[1];
             console.log("Account:", window.account);
             return web3.eth.getBalancePromise(window.account);
         })
+        // window account to populate Bob's balance
         .then(balance => $("#balanceBob").html(web3.fromWei(balance.toString(10))
         ))
         .catch(console.error);
 });
 
+// Upon loading of the site 
 window.addEventListener('load', function () {
+    //check if accounts are available
     return web3.eth.getAccountsPromise()
         .then(accounts => {
+            //when no accounts available, return error
             if (accounts.length == 0) {
                 $("#balanceCarol").html("N/A");
                 throw new Error("No account with which to transact");
             }
+            //if accounts available, set the third one to window account
             window.account = accounts[2];
             console.log("Account:", window.account);
             return web3.eth.getBalancePromise(window.account);
         })
+        // window account to populate Carol's balance
         .then(balance => $("#balanceCarol").html(web3.fromWei(balance.toString(10))
         ))
         .catch(console.error);
 });
 
+// Upon loading of the site check the balance of the contract and return to populate 
 window.addEventListener('load', function () {
     return Splitter.deployed()
         .then(function (split) {
@@ -80,23 +98,29 @@ window.addEventListener('load', function () {
         .catch(console.error);
 });
 
+// function that allows Alice to deposit from her account to the Splitter contract
 const deposit = function () {
     let split;
+    //return all accounts
     return web3.eth.getAccountsPromise()
         .then(accounts => {
+            //if no accounts returned, show error message
             if (accounts.length == 0) {
                 $("#balanceAlice").html("N/A");
                 throw new Error("No account with which to transact");
             }
+            //set Alice's account as the window account
             window.account = accounts[0];
             console.log("Account to withdraw from:", window.account);
             return Splitter.deployed()
+            //deposit the entered amount of Ether to the contract, where the contract will split this between the entered recipient1 and recipient2
                 .then(_split => {
                     split = _split;
                     console.log("Splitter address: ", split.address);
                     console.log(split)
                     return split.deposit.sendTransaction($("input[name='recipient1']").val(), $("input[name='recipient2']").val(), { from: window.account, value: web3.toWei($("input[name='amount']").val(), "ether"), gas:'210000' });
                 })
+                //return the transaction hash
                 .then(txHash => {
                     $("#status").html("Transaction on the way " + txHash);
                     const tryAgain = () => web3.eth.getTransactionReceiptPromise(txHash)
@@ -105,6 +129,7 @@ const deposit = function () {
                             Promise.delay(1000).then(tryAgain));
                     return tryAgain();
                 })
+                //return a success or failure of the transaction
                 .then(receipt => {
                     if (parseInt(receipt.status) != 1) {
                         console.error("Wrong status");
@@ -125,11 +150,13 @@ const deposit = function () {
                     }
                     return web3.eth.getBalancePromise(window.account);
                 })
+                // show the updated balance of Alice 
                 .then(balance => {
                     $("#balanceAlice").html(web3.fromWei(balance.valueOf()));
                     //set the new contract balance
                     return split.getContractBalance();
                 })
+                //show the updated balance of the contract
                 .then(balance => {
                     $("#balanceContract").html(web3.fromWei(balance.valueOf()));
                 })
@@ -140,6 +167,7 @@ const deposit = function () {
         })
 };
 
+//Bob withdraws, creating an input for the 'withdrawFunds' function
 const withdrawFundsBob = function () {
     return web3.eth.getAccountsPromise()
         .then(accounts => {
@@ -147,6 +175,7 @@ const withdrawFundsBob = function () {
         })    
 }
 
+//Carol withdraws, creating an input for the 'withdrawFunds' function
 const withdrawFundsCarol = function () {
     return web3.eth.getAccountsPromise()
         .then(accounts => {
@@ -154,14 +183,17 @@ const withdrawFundsCarol = function () {
         })    
 }
 
+// function to withdrawfunds by either Carol or Bob (their accounts are an input for this function)
 const withdrawFunds = function (accountToWithdraw) {
     let split;
     console.log(accountToWithdraw);
     return Splitter.deployed()
+    //Run the withdrawFunds function in the contract for one of the recipients
         .then(_split => {
             split = _split;
             return split.withdrawFunds.sendTransaction({ from: accountToWithdraw });
         })
+        //return the transaction hash
         .then(txHash => {
             $("#status").html("Transaction on the way " + txHash);
             const tryAgain = () => web3.eth.getTransactionReceiptPromise(txHash)
@@ -170,6 +202,7 @@ const withdrawFunds = function (accountToWithdraw) {
                     Promise.delay(1000).then(tryAgain));
             return tryAgain();
         })
+        //return a success of failure message
         .then(receipt => {
             if (parseInt(receipt.status) != 1) {
                 console.error("Wrong status");
--- test/splitter.js
+++ test/splitter.js
@@ -12,9 +12,6 @@ contract('Splitter', function () {
   });
 })
 
-
-
-
 contract('Splitter', function () {
   it("should return balance of contract deployed", function () {
     return Splitter.deployed().then(function (instance) {
@@ -26,42 +23,22 @@ contract('Splitter', function () {
   });
 })
 
-
-
 contract('Splitter', function (accounts) {
-    owner = accounts[0];
-    receiver1 = accounts[1];
-    receiver2 = accounts[2];
-    it("owner can deposit to contract", function () {
+  owner = accounts[0];
+  receiver1 = accounts[1];
+  receiver2 = accounts[2];
+  it("owner can deposit to contract", function () {
     var amount = 3;
     return Splitter.deployed().then(function (instance) {
       splitter = instance;
       return splitter.deposit(receiver1, receiver2, { from: owner, value: amount });
     }).then(function () {
-       return splitter.getContractBalance.call(splitter);
+      return splitter.getContractBalance.call(splitter);
     }).then(function (balanceC) {
-       assert.equal(balanceC.toString(10), "3", "The contract balance is incorrect");
+      assert.equal(balanceC.toString(10), "3", "The contract balance is incorrect");
     });
   });
 })
-
-// contract('Splitter', function (accounts) {
-//   owner = accounts[0];
-//   receiver1 = accounts[1];
-//   receiver2 = accounts[2];
-//   balanceReceiver1 = 1.5;
-//   it("verifies that receiver can withdraw funds", function () {
-//   var amount = 1.5;
-//   return Splitter.deployed().then(function (instance) {
-//     splitter = instance;
-//     return splitter.withdrawFunds({from: accounts[1]});
-//   }).then(function () {
-//     return splitter.getContractBalance.call(splitter);
-//   }).then(function (balanceC) {
-//      assert.equal(balanceC.toString(10), "3", "0 is not the balance of the contract");
-//   });
-// });
-// })
 
 //TESTS USING ASYNC
 
@@ -71,7 +48,7 @@ contract('Splitter', function () {
     let address = await split.address;
     console.log(address);
   })
-})  
+})
 
 contract('Splitter', function () {
   it("should return balance of contract deployed - Async", async function () {
@@ -79,7 +56,7 @@ contract('Splitter', function () {
     let balance = await split.getContractBalance.call(split);
     console.log(balance.toString(10));
     assert.equal(balance.toString(10), "0", "0 is not the balance of the contract");
-    });
+  });
 })
 
 contract('Splitter', function (accounts) {
@@ -96,4 +73,58 @@ contract('Splitter', function (accounts) {
 })
 
 
+contract('Splitter', async (accounts) => {
+    let owner = accounts[0];
+    let receiver1 = accounts[1];
+    let receiver2 = accounts[2];
+ 
+    
+    it("verify everyting is set up correctly and set split - Async", async () => {
+        // Retrieve the splitter contract instance
+        let split = await Splitter.deployed();
+        // Should verify the contract balance is Zero
+        assert.equal(0, await split.getContractBalance().valueOf(), "The contract balance needs to be 0");
+      })
 
+    it("owner can deposit to contract - Async", async () => {
+        // Amount of ether in wei to deposit
+        var amountToDeposit = 2000000;
+        let split = await Splitter.deployed();
+        // Retrieve the current balance of the contract
+        var oldBalance = await split.getContractBalance.call();
+        // Execute the deposit and save the transaction receipt
+        var receipt = await split.deposit(receiver1, receiver2, { from: owner, value: amountToDeposit, gas: 210000 });
+        // Test the results returned in the transaction receipt (e.g. Event log files)
+        assert.equal(receipt.logs[0].args.sender, owner, "Owner is incorrect");          
+        assert.equal(receipt.logs[0].args.deposit.valueOf(), amountToDeposit, "Deposit amount event log file entry is incorrect");
+        assert.equal(receipt.logs[0].args.receiver1, receiver1, "Receiver 1 event log file entry is incorrect");
+        assert.equal(receipt.logs[0].args.receiver2, receiver2, "Receiver 2 event log file entry is incorrect");
+        //Get the new balance of the splitter contract
+        let newBalance = await split.getContractBalance();
+        // Verify the new balance is correct (sum of old balance plus whatever has been added)
+        assert.equal(newBalance.valueOf(), +amountToDeposit + +oldBalance.valueOf(), "The new contract balance is incorrect");
+      });
+
+    it("receiver1 can withdraw from contract - Async", async () => {
+      //amount of ether to withdraw
+      let split = await Splitter.deployed();
+      let gasused = 21024;
+      const oldContractBalance = await split.getContractBalance.call();
+      console.log("Old contract balance:", oldContractBalance.valueOf());
+      const receiver1_starting_balance = await web3.eth.getBalance(receiver1);
+      console.log("Receiver 1 starting balance:", receiver1_starting_balance.valueOf());
+      let withdraw = await split.withdrawFunds({from: receiver1});
+      const receiver1_end_balance = await web3.eth.getBalance(receiver1);
+      console.log("Receiver 1 end balance:", receiver1_end_balance.valueOf());
+      const newContractBalance = await split.getContractBalance.call();
+      console.log("New Contract Balance:", newContractBalance.valueOf());
+      const amountWithdrawn = oldContractBalance - newContractBalance;
+      console.log("Amount withdrawn:", amountWithdrawn.valueOf());
+      const newReceiver1Balance = receiver1_starting_balance + amountWithdrawn - gasused;
+      console.log("New balance Receiver1:", newReceiver1Balance.valueOf());
+      // Verify that receiver1 has the correct balance after withdrawing
+      assert.equal(receiver1_end_balance, receiver1_starting_balance + amountWithdrawn - gasused, "The balance of receiver1 after withdrawal is incorrect");
+      // Verify that the contract has the correct balance after withdrawing
+      assert.equal(newContractBalance, oldContractBalance - amountWithdrawn, "New contract balance is incorrect" );
+       })
+  })
