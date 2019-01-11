3d615885f75bfd85f788e1e814246156db53a52d
Richard-Boeve <richardboeve@hotmail.com>
2019-01-11 17:50:16 +1300

    contract updated: mapping, state, uneven amount

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,63 +2,71 @@ pragma solidity 0.4.24;
 
 contract Splitter {
     
-    address public aliceOwner;
-    address public bob;
-    address public carol;
-    uint256 balanceBob;
-    uint256 balanceCarol;
-    
+    address public owner;
 
-    event LogDeposit(address sender, uint256 deposit, address receiver1, address receiver2);
-    event LogWithdrawFunds(address sender);
-	
+    enum SplitterState {
+        Operational,
+        Paused,
+        Deactivated
+    }
+    
+    event LogDeposit(address sender, uint256 depositAmount, uint256 amountToBeDivided, uint256 returnAmount, address receiver1, address receiver2);
+    event LogWithdrawFunds(address sender, uint amount);
+    
+    // mapping of address to balance
+    mapping(address => uint256) public balanceReceiver;
+    
+    // Variable to store the current state of this contract
+    SplitterState public state;
+
     constructor() public payable {
-        aliceOwner = msg.sender;
+        owner = msg.sender;
+        state = SplitterState.Operational;
     }
-	
-    function getContractBalance() public view returns (uint256) {
-        return address(this).balance;
+    
+    // Function that allows owner to change the state of the contract
+    function setState(SplitterState newState) public {
+        //Verify that the sender is the owner
+        require(msg.sender == owner, "Only owner can set the state of the contract");
+        //Verify if the state is Deactivated, if so, don't allow update to the state;
+        require(state != SplitterState.Deactivated, "The contract is deactivated and can't be made operational or paused");
+        //Set the state of the Contract
+        state = newState;
     }
-
-
-    function deposit(address receiver1, address receiver2) public payable returns (bool){
-        require(msg.sender == aliceOwner,"Only Alice can deposit Ether");
+    
+    function deposit(address _receiver1, address _receiver2) public payable {
+        // Verify that the contract is operational before continueing
+        require(state == SplitterState.Operational, "Contract is not operational");
         // Verify non empty addresses have been provided
-        require(receiver1 != address(0), "Must provide two addresses");
-        require(receiver2 != address(0), "Must provide two addresses");
+        require(_receiver1 != address(0), "Must provide two addresses");
+        require(_receiver2 != address(0), "Must provide two addresses");
         // Verify receiver 1 address is not equal to receiver2 address  
-        require(receiver1 != receiver2, "Must provide 2 different addresses");
-        // Verify if this is second deposit that the addresses provided are still the same
-        if ((bob == address(0) && (carol == address(0)))) {
-            // First time of deposit operation set bob and carol addresses
-            bob = receiver1;
-            carol = receiver2;
-            }
-        else {
-            // Not first time deposit check the addresses provided are still correct
-            require(bob == receiver1 || bob == receiver2,"Address for bob provided is invalid");
-            require(carol == receiver1 || carol == receiver2,"Address for carol provided is invalid");
-            }
-        emit LogDeposit(msg.sender, msg.value, receiver1, receiver2);
-        balanceBob += msg.value /2;
-        balanceCarol += msg.value /2;
-        return true;
+        require(_receiver1 != _receiver2, "Must provide 2 different addresses");
+
+        // If the msg.value is an uneven number
+        if (msg.value % 2 != 0) {
+            // Credit 1 wei to the address of the sender
+            balanceReceiver[msg.sender] += 1; 
+        }
+        // Remaining msg.value to be split between _receiver1 and _receiver2
+        // If the msg.value is an uneven number, msg.value will be divided by two and the remainder will disappear
+        balanceReceiver[_receiver1] += msg.value / 2;
+        balanceReceiver[_receiver2] += msg.value / 2;
+        emit LogDeposit(msg.sender, msg.value, msg.value / 2, msg.value % 2, _receiver1, _receiver2);
     }
     
-    function withdrawFunds() public returns (uint256) {
-        uint amountBob = balanceBob;
-        uint amountCarol = balanceCarol;
-        if (msg.sender == bob) {
-            balanceBob = 0;    
-            address(bob).transfer(amountBob);
-            emit LogWithdrawFunds (msg.sender);
-        } else if (msg.sender == carol) {
-            balanceCarol = 0;
-            address(carol).transfer(amountCarol);
-            emit LogWithdrawFunds (msg.sender);
-          } else { revert(); }
-    }
-    
-    function() external {
+    function withdrawFunds() public {
+        // Verify that the contract isn't paused
+        require(state != SplitterState.Paused, "Contract must be operational or deactivated to be able to withdraw");
+        // Set the balance of the msg.sender to 0
+        uint256 amountForWithdrawal = balanceReceiver[msg.sender];
+        balanceReceiver[msg.sender] = 0;
+        // Transfer the balance to the msg.sender
+        address(msg.sender).transfer(amountForWithdrawal);
+        // Create logs
+        emit LogWithdrawFunds (msg.sender, amountForWithdrawal);
+    }    
+
+        function() external {
     }
 }
