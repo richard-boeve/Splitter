801efe29bcdc311d7424f48f7c2b855c33c75611
Richard-Boeve <richardboeve@hotmail.com>
2019-01-17 15:07:25 +1300

    contract updated with comments Rob & Xavier

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,38 +2,49 @@ pragma solidity 0.4.24;
 
 contract Splitter {
     
+    //Global Variables
     address public owner;
+    SplitterState public state;
+    uint256 half;
 
+    //Constructor, setting initial properties
+    constructor() public {
+        owner = msg.sender;
+        state = SplitterState.Operational;
+    }
+
+    //Modifiers
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
+
+    //Defining the possible states of the contract
     enum SplitterState {
         Operational,
         Paused,
         Deactivated
     }
     
-    event LogDeposit(address sender, uint256 depositAmount, uint256 amountToBeDivided, uint256 returnAmount, address receiver1, address receiver2);
-    event LogWithdrawFunds(address sender, uint amount);
+    //All events that can be logged
+    event LogDeposit(address indexed sender, uint256 depositAmount, address indexed receiver1, address indexed receiver2);
+    event LogWithdrawFunds(address indexed sender, uint amount);
+    event LogSetState(SplitterState newState);
     
-    // mapping of address to balance
+    //Mapping of address to balance
     mapping(address => uint256) public balanceReceiver;
-    
-    // Variable to store the current state of this contract
-    SplitterState public state;
 
-    constructor() public payable {
-        owner = msg.sender;
-        state = SplitterState.Operational;
-    }
-    
     // Function that allows owner to change the state of the contract
-    function setState(SplitterState newState) public {
-        //Verify that the sender is the owner
-        require(msg.sender == owner, "Only owner can set the state of the contract");
+    function setState(SplitterState newState) public onlyOwner {
         //Verify if the state is Deactivated, if so, don't allow update to the state;
         require(state != SplitterState.Deactivated, "The contract is deactivated and can't be made operational or paused");
         //Set the state of the Contract
         state = newState;
+        //Create logs
+        emit LogSetState(newState);
     }
     
+    //Allows the message sender to send Ether to the contract and have it assigned to the balance of two receiver addresses
     function deposit(address _receiver1, address _receiver2) public payable {
         // Verify that the contract is operational before continueing
         require(state == SplitterState.Operational, "Contract is not operational");
@@ -42,7 +53,6 @@ contract Splitter {
         require(_receiver2 != address(0), "Must provide two addresses");
         // Verify receiver 1 address is not equal to receiver2 address  
         require(_receiver1 != _receiver2, "Must provide 2 different addresses");
-
         // If the msg.value is an uneven number
         if (msg.value % 2 != 0) {
             // Credit 1 wei to the address of the sender
@@ -50,11 +60,13 @@ contract Splitter {
         }
         // Remaining msg.value to be split between _receiver1 and _receiver2
         // If the msg.value is an uneven number, msg.value will be divided by two and the remainder will disappear
-        balanceReceiver[_receiver1] += msg.value / 2;
-        balanceReceiver[_receiver2] += msg.value / 2;
-        emit LogDeposit(msg.sender, msg.value, msg.value / 2, msg.value % 2, _receiver1, _receiver2);
+        half = msg.value / 2;
+        balanceReceiver[_receiver1] += half;
+        balanceReceiver[_receiver2] += half;
+        emit LogDeposit(msg.sender, msg.value, _receiver1, _receiver2);
     }
-    
+
+    //Allow the message sender to retrieve his/her funds
     function withdrawFunds() public {
         // Verify that the contract isn't paused
         require(state != SplitterState.Paused, "Contract must be operational or deactivated to be able to withdraw");
@@ -66,7 +78,8 @@ contract Splitter {
         // Create logs
         emit LogWithdrawFunds (msg.sender, amountForWithdrawal);
     }    
-
-        function() external {
-    }
+    
+    //Fallback function which rejects funds sent to the contract address if sender is not the owner
+    function() public { revert(); 
+    }  
 }
