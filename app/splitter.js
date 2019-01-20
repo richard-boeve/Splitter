8cf8847a2161ef99be6a89799413350c726fe983
Richard-Boeve <richardboeve@hotmail.com>
2019-01-20 13:44:51 +1300

    updated the front end to work with revised contract - now works for any sender/receiver

--- app/splitter.js
+++ app/splitter.js
@@ -7,86 +7,20 @@ const splitterJson = require("../build/contracts/Splitter.json");
 require("file-loader?name=./index.html!./index.html");
 
 // Use a web3 browser if availble
-if (typeof web3 !== 'undefined') {
-    console.log('Web3 browser detected! ' + web3.currentProvider.constructor.name)
-    web3 = new Web3(web3.currentProvider);
-// Otherwise, use a own provider with port 8545  
-} else {
-    console.log('Web3 browser not detected, setting own provider!')
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-}
+    if (typeof web3 !== 'undefined') {
+        console.log('Web3 browser detected! ' + web3.currentProvider.constructor.name)
+        web3 = new Web3(web3.currentProvider);
+    // Otherwise, use a own provider with port 8545  
+    } else {
+        console.log('Web3 browser not detected, setting own provider!')
+        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
 
 Promise.promisifyAll(web3.eth, { suffix: "Promise" });
 Promise.promisifyAll(web3.version, { suffix: "Promise" });
 
 const Splitter = contract(splitterJson);
 Splitter.setProvider(web3.currentProvider);
-
-// Upon loading of the site 
-window.addEventListener('load', function () {
-    //check if accounts are available
-    return web3.eth.getAccountsPromise()
-        .then(accounts => {
-            console.log(accounts);
-            //when no accounts available, return error
-            if (accounts.length == 0) {
-                $("#balanceAlice").html("N/A");
-                throw new Error("No account with which to transact");
-            }
-            //if accounts available, set the first one to window account
-            window.account = accounts[0];
-            console.log("Account:", window.account);
-            return web3.eth.getBalancePromise(window.account);
-        })
-        // window account to populate Alice's balance
-        .then(balance => $("#balanceAlice").html(web3.fromWei(balance.valueOf())))
-        .then(() => $("#send").click(deposit))
-        .then(() => $("#withdrawBob").click(withdrawFundsBob))
-        .then(() => $("#withdrawCarol").click(withdrawFundsCarol))
-        .catch(console.error);
-});
-
-// Upon loading of the site 
-window.addEventListener('load', function () {
-    //check if accounts are available
-    return web3.eth.getAccountsPromise()
-        .then(accounts => {
-            //when no accounts available, return error
-            if (accounts.length == 0) {
-                $("#balanceBob").html("N/A");
-                throw new Error("No account with which to transact");
-            }
-            //if accounts available, set the second one to window account
-            window.account = accounts[1];
-            console.log("Account:", window.account);
-            return web3.eth.getBalancePromise(window.account);
-        })
-        // window account to populate Bob's balance
-        .then(balance => $("#balanceBob").html(web3.fromWei(balance.toString(10))
-        ))
-        .catch(console.error);
-});
-
-// Upon loading of the site 
-window.addEventListener('load', function () {
-    //check if accounts are available
-    return web3.eth.getAccountsPromise()
-        .then(accounts => {
-            //when no accounts available, return error
-            if (accounts.length == 0) {
-                $("#balanceCarol").html("N/A");
-                throw new Error("No account with which to transact");
-            }
-            //if accounts available, set the third one to window account
-            window.account = accounts[2];
-            console.log("Account:", window.account);
-            return web3.eth.getBalancePromise(window.account);
-        })
-        // window account to populate Carol's balance
-        .then(balance => $("#balanceCarol").html(web3.fromWei(balance.toString(10))
-        ))
-        .catch(console.error);
-});
 
 // Upon loading of the site check the balance of the contract and return to populate 
 window.addEventListener('load', function () {
@@ -95,98 +29,65 @@ window.addEventListener('load', function () {
             return split.getContractBalance.call();
         }).then(balance => $("#balanceContract").html(web3.fromWei(balance.toString(10))
         ))
+        .then(() => $("#send").click(deposit))
+        .then(() => $("#withdraw").click(withdrawFunds))
         .catch(console.error);
 });
 
-// function that allows Alice to deposit from her account to the Splitter contract
+//Function that allows a deposit (for two receivers) to be made to the contract
 const deposit = function () {
     let split;
-    //return all accounts
-    return web3.eth.getAccountsPromise()
-        .then(accounts => {
-            //if no accounts returned, show error message
-            if (accounts.length == 0) {
-                $("#balanceAlice").html("N/A");
-                throw new Error("No account with which to transact");
-            }
-            //set Alice's account as the window account
-            window.account = accounts[0];
-            console.log("Account to withdraw from:", window.account);
-            return Splitter.deployed()
-            //deposit the entered amount of Ether to the contract, where the contract will split this between the entered recipient1 and recipient2
-                .then(_split => {
-                    split = _split;
-                    console.log("Splitter address: ", split.address);
-                    console.log(split)
-                    return split.deposit.sendTransaction($("input[name='recipient1']").val(), $("input[name='recipient2']").val(), { from: window.account, value: web3.toWei($("input[name='amount']").val(), "ether"), gas:'210000' });
-                })
-                //return the transaction hash
-                .then(txHash => {
-                    $("#status").html("Transaction on the way " + txHash);
-                    const tryAgain = () => web3.eth.getTransactionReceiptPromise(txHash)
-                        .then(receipt => receipt !== null ?
-                            receipt :
-                            Promise.delay(1000).then(tryAgain));
-                    return tryAgain();
-                })
-                //return a success or failure of the transaction
-                .then(receipt => {
-                    if (parseInt(receipt.status) != 1) {
-                        console.error("Wrong status");
-                        console.error(receipt);
-                        $("#status").html("There was an error in the tx execution, status not 1");
-                    } else if (receipt.logs.length == 0) {
-                        console.error("Empty logs");
-                        console.error(receipt);
-                        $("#status").html("There was an error in the tx execution");
-                    } else {
-                        var help = split.LogDeposit().formatter(receipt.logs[0]);
-                        console.log("Sender's address: " + help.args.sender);
-                        console.log("Deposit amount: " + web3.fromWei(help.args.deposit));
-                        console.log("Receiver 1: " + help.args.receiver1);
-                        console.log("Receiver 2: " + help.args.receiver2);
-                        //console.log(help.args.deposit);
-                        $("#status").html("Transfer executed");
-                    }
-                    return web3.eth.getBalancePromise(window.account);
-                })
-                // show the updated balance of Alice 
-                .then(balance => {
-                    $("#balanceAlice").html(web3.fromWei(balance.valueOf()));
-                    //set the new contract balance
-                    return split.getContractBalance();
-                })
-                //show the updated balance of the contract
-                .then(balance => {
-                    $("#balanceContract").html(web3.fromWei(balance.valueOf()));
-                })
-                .catch(e => {
-                    $("#status").html(e.toString());
-                    console.error(e);
-                });
+    window.account = $("input[name='sender']").val();
+    return Splitter.deployed()
+        //deposit the entered amount of Ether to the contract, where the contract will split this between the entered recipient1 and recipient2
+        .then(_split => {
+            split = _split;
+            console.log("Splitter address: ", split.address);
+            return split.deposit.sendTransaction($("input[name='recipient1']").val(), $("input[name='recipient2']").val(), { from: window.account, value: web3.toWei($("input[name='amount']").val(), "ether"), gas: '210000' });
         })
-};
-
-//Bob withdraws, creating an input for the 'withdrawFunds' function
-const withdrawFundsBob = function () {
-    return web3.eth.getAccountsPromise()
-        .then(accounts => {
-            withdrawFunds(accounts[1]);
-        })    
+        //return the transaction hash
+        .then(txHash => {
+            $("#status").html("Transaction on the way " + txHash);
+            const tryAgain = () => web3.eth.getTransactionReceiptPromise(txHash)
+                .then(receipt => receipt !== null ?
+                    receipt :
+                    Promise.delay(1000).then(tryAgain));
+            return tryAgain();
+        })
+        //return a success or failure of the transaction
+        .then(receipt => {
+            if (parseInt(receipt.status) != 1) {
+                console.error("Wrong status");
+                console.error(receipt);
+                $("#status").html("There was an error in the tx execution, status not 1");
+            } else if (receipt.logs.length == 0) {
+                console.error("Empty logs");
+                console.error(receipt);
+                $("#status").html("There was an error in the tx execution");
+            } else {
+                var logDeposit = split.LogDeposit().formatter(receipt.logs[0]);
+                console.log("Sender's address: " + logDeposit.args.sender);
+                console.log("Amount deposited: " + web3.fromWei(logDeposit.args.depositAmount));
+                console.log("Receiver 1: " + logDeposit.args.receiver1);
+                console.log("Receiver 2: " + logDeposit.args.receiver2);
+                $("#status").html("Transfer executed");
+            }
+            return split.getContractBalance();
+        })
+        //show the updated balance of the contract
+        .then(balance => {
+            $("#balanceContract").html(web3.fromWei(balance.valueOf()));
+        })
+        .catch(e => {
+            $("#status").html(e.toString());
+            console.error(e);
+        });
 }
 
-//Carol withdraws, creating an input for the 'withdrawFunds' function
-const withdrawFundsCarol = function () {
-    return web3.eth.getAccountsPromise()
-        .then(accounts => {
-            withdrawFunds(accounts[2]);
-        })    
-}
-
-// function to withdrawfunds by either Carol or Bob (their accounts are an input for this function)
-const withdrawFunds = function (accountToWithdraw) {
+// function that allows the inputted address to withdraw its funds from the contract
+const withdrawFunds = function () {
     let split;
-    console.log(accountToWithdraw);
+    accountToWithdraw = $("input[name='withdrawalAddress']").val();
     return Splitter.deployed()
     //Run the withdrawFunds function in the contract for one of the recipients
         .then(_split => {
@@ -213,9 +114,20 @@ const withdrawFunds = function (accountToWithdraw) {
                 console.error(receipt);
                 $("#status").html("There was an error in the tx execution");
             } else {
-                console.log(split.LogWithdrawFunds().formatter(receipt.logs[0]).args);
+                var logWithdraw = split.LogWithdrawFunds().formatter(receipt.logs[0]);
+                console.log("Sender's address: " + logWithdraw.args.sender);
+                console.log("Amount withdrawn: " + web3.fromWei(logWithdraw.args.amount));    
                 $("#status").html("Transfer executed");
             }
-        });
+            return split.getContractBalance();
+        })
+        //show the updated balance of the contract
+        .then(balance => {
+        $("#balanceContract").html(web3.fromWei(balance.valueOf()));
+        })
+        .catch(e => {
+        $("#status").html(e.toString());
+        console.error(e);
+        });;
 };
 
