4662443a44d089163fc4e155d2cc45449f9cdadc
Richard-Boeve <richardboeve@hotmail.com>
2019-01-27 16:14:06 +1300

    changes to Ownded.sol including get and change owner functions

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -3,21 +3,31 @@ pragma solidity 0.4.24;
 contract Owned {
     
     //Global variables
-    address public owner;
+    address private owner;
     
     //Constructor, setting the owner upon contract creation
     constructor() public {
         owner = msg.sender;
     }
     
+    //Events
+    event LogChangeOwner(address indexed oldOwner, address indexed newOwner); 
+    
     //Modifiers
     modifier onlyOwner {
         require(msg.sender == owner, "Only owner can perform this action");
         _;
     }
-
-    //Fallback function which rejects funds sent to the contract address if sender is not the owner
-    function() public { 
-        revert("Fallback function triggered"); 
-    }  
+    
+    //Function that queries the current owner
+    function getOwner() public view returns(address) {
+        return owner;
+    }
+    
+    //Function that changes the owner
+    function changeOwner(address _newOwner) public onlyOwner {
+        require(_newOwner != 0, "No address provided");
+        owner = _newOwner;
+        emit LogChangeOwner(msg.sender, _newOwner);
+    }
 }
